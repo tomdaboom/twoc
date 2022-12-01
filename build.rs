da8af340b5dd902836990a8f33068c5151d236b1
Tom <tom.divers9@gmail.com>
2022-12-01 00:11:36 +0000

    added build.rs

---
+++ build.rs
@@ -0,0 +1,7 @@
+// Taken from the lalrpop tutorial: https://lalrpop.github.io/lalrpop/tutorial/001_adding_lalrpop.html
+
+extern crate lalrpop;
+
+fn main() {
+    lalrpop::process_root().unwrap();
+}
