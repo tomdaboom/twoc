4abfbc61b04144b039b03d20b44ffda73719f1ed
Tom <tom.divers9@gmail.com>
2022-12-02 14:04:16 +0000

    reorganised src file structure

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,26 @@
+pub mod parser;
+
 #[macro_use] extern crate lalrpop_util;
 
-pub mod ast;
+use parser::ast;
 
-lalrpop_mod!(pub parser);
+lalrpop_mod!(pub grammar_rules, "/parser/grammar_rules.rs");
 
 fn main() {
     // Declare Stmt parser
-    let stmt_list_parser = parser::StmtListParser::new();
+    let stmt_list_parser = grammar_rules::StmtListParser::new();
 
     // Declare test string
-    let test_prog = "while ( read == 'C' || read == 'C' && !(c == 0) ) { }";
+    let test_prog = "while ( read == 'C' || read == 'C' && !(c == 0) ) { c -= 12; }";
 
     // Parse test string
-    let test = stmt_list_parser.parse(test_prog).unwrap();
+    let test = stmt_list_parser.parse(test_prog);
 
-    // Print test ast
-    println!("AST:\n{:?}", test);
+    match test {
+        Ok(ref ast) => 
+            println!("AST:\n{:?}", ast),
+
+        Err(_) => 
+            println!("Parse erorr detected!"),
+    }
 }
---
+++ src/parser/mod.rs
@@ -0,0 +1,3 @@
+pub mod ast;
+
+pub mod err_reporter;
