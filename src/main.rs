9897bb0cd6e9cd3e54eded3fc85bcdc974e7cadb
Tom <tom.divers9@gmail.com>
2023-01-10 14:10:47 +0000

    input string changed

--- src/main.rs
+++ src/main.rs
@@ -54,7 +54,7 @@ fn main() {
     autom.print();
 
     // Test that the automaton accepts an example word via the glueck procedure
-    let test_word = "11000";
+    let test_word = "10011010";
     let accepting = glueck::glueck_procedure(autom, test_word);
 
     if accepting {
