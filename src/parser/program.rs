f6551376178c5767374e18ed0541dc2d3f87afb2
Tom <tom.divers9@gmail.com>
2023-01-10 14:26:53 +0000

    included method to check if a program branches

--- src/parser/program.rs
+++ src/parser/program.rs
@@ -32,5 +32,43 @@ impl Program {
             print!("{}", stmt.print(2));
         }
     }
+
+    // Check if this program is deterministic
+    pub fn deterministic(&self) -> bool {
+        return Program::no_branches(self.stmts.clone());
+    }
+
+    // True if prog contains no branches, false otherwise
+    fn no_branches(prog : Vec<ast::Stmt>) -> bool {
+        for stmt in prog {
+            match stmt {
+                // Return false if we find a branch statement
+                ast::Stmt::Branch(_) => return false,
+
+                // Recursively check each of the branches of an if statement
+                ast::Stmt::If(_, if_branch, else_branch) => { 
+                    if !Program::no_branches(if_branch) || !Program::no_branches(else_branch) {
+                        return false;
+                    }
+                    
+                    continue;
+                },
+
+                // Recursively check a while statement
+                ast::Stmt::While(_, while_branch) => {
+                    if !Program::no_branches(while_branch) {
+                        return false;
+                    }
+
+                    continue;
+                },
+
+                // Otherwise, continue
+                _ => continue,
+            }
+        }
+
+        true
+    }
 }
 
