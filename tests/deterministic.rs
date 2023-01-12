f03f0d59f0acff8faa01a5bc66daaf1c5303bbbd
Tom <tom.divers9@gmail.com>
2023-01-12 13:35:07 +0000

    test for zeros then ones

--- tests/deterministic.rs
+++ tests/deterministic.rs
@@ -50,4 +50,43 @@ mod determ_tests {
         }
 
     }
+
+    #[test]
+    pub fn zeros_then_ones() {
+        // Declare parser for Twoc rule
+        let parser = grammar_rules::TwocParser::new();
+
+        // Load file
+        let test_prog = fs::read_to_string("./twocprogs/zeros_then_ones.twoc").expect("File not found");
+
+        // Parse the file
+        let test = parser.parse(&test_prog);
+        let mut prog = match test {
+            // Output any parse errors
+            Err(ref err) => panic!("Parse Error:\n{:?}", err),
+            Ok(prog) => prog,
+        };
+
+        // Contract the AST
+        prog.contract();
+
+        // Construct the automaton from the program
+        let autom = determ_construction::construct_from_prog(prog);
+
+        // Some test examples
+        let test_words = [
+            ("00000000001111111111", true), 
+            ("00001111", true),
+            ("0001111", false),
+            ("000001111", false),
+            ("1010101010101010", false),
+            ("1001010101101010111111010101", false),
+        ];
+
+        // Check that each of the words gives the correct answer
+        for (word, expected) in test_words {
+            let glueck_output = glueck_procedure(autom.clone(), word);
+            assert_eq!(glueck_output, expected);
+        }
+    }
 }
