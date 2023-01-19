826b7cf9adbafde402bb53adf1b63bf914054c76
Tom <tom.divers9@gmail.com>
2023-01-19 16:26:52 +0000

    added simple results plotter script

---
+++ tests/bench_results/result_plotter.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+ns  = []
+ts  = []
+dts = []
+
+with open(sys.argv[1], 'r') as file:
+    for line in file:
+        split = line.split(",")
+        ns.append(int(split[0]))
+        ts.append(float(split[1]))
+        dts.append(float(split[2]))
+
+dts[0] = 0
+
+plt.plot(ns, ts)
+plt.xlabel('n')
+plt.ylabel('t')
+plt.xticks(np.arange(min(ns), max(ns), (max(ns)-min(ns))/5))
+plt.yticks(np.arange(min(ts), max(ts), (max(ts)-min(ts))/5))
+plt.show()
+
+plt.plot(ns, dts)
+plt.xlabel('n')
+plt.ylabel('dt')
+plt.xticks(np.arange(min(ns), max(ns), 2000))
+plt.yticks(np.arange(min(dts), max(dts), (max(dts)-min(dts))/5))
+plt.show()
