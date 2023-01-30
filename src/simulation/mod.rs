78606d59ef5ed1622ff96c3aa978d90c56a62144
Tom <tom.divers9@gmail.com>
2023-01-30 17:54:30 +0000

    initial commit for rytter

--- src/simulation/mod.rs
+++ src/simulation/mod.rs
@@ -3,3 +3,4 @@ pub mod config;
 pub mod glueck;
 //pub mod jones;
 pub mod ahu;
+pub mod rytter;
---
+++ src/simulation/rytter.rs
@@ -0,0 +1,26 @@
+use std::collections::VecDeque;
+
+use hashbrown::HashSet;
+
+use crate::automaton::autom::{Autom, Transition};
+use crate::automaton::generic_autom::State;
+use crate::simulation::config::Config;
+use crate::parser::ast::{Readable, Input};
+
+pub type StrIndex = i32;
+
+pub fn rytter_procedure<'a>(autom : &'a Autom, input : &str) -> bool {
+    false
+}
+
+struct RytterSimulator<'a> {
+    autom : &'a Autom,
+
+    input : Input,
+
+    n : StrIndex,
+
+    queue : VecDeque<(StrIndex, StrIndex)>,
+
+
+}
