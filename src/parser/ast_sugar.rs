1c5e6ee4ad2a53fd2d5a300f2ddb120af37298a4
Tom <tom.divers9@gmail.com>
2023-01-31 13:24:06 +0000

    basically nothing

--- src/parser/ast_sugar.rs
+++ src/parser/ast_sugar.rs
@@ -1,4 +1,4 @@
-
+use crate::parser::ast::Cond;
 
 // AST nodes for statements 
 #[derive(Debug, Clone)]
@@ -14,6 +14,9 @@ pub enum Stmt {
 
     // c += j
     Incr(i32), 
+
+    // c = j
+    Asgn(i32),
 
     // (move(i), c += j), only present after contraction
     BasicBlock(i32, i32),
@@ -123,105 +126,12 @@ impl Stmt {
                     }
                 }
             }
+
+            _ => panic!("Can't print this kind of statement yet!"),
             
         }
 
         // Return
         out
-    }
-}
-
-// AST nodes for conditions
-#[derive(Debug, Clone)]
-pub enum Cond {
-    // read == X
-    Read(Readable),
-
-    // read != X
-    NotRead(Readable),
-
-    // c == 0
-    CheckZero(),
-
-    // c != 0
-    CheckNotZero(),
-
-    // X && Y
-    And(Box<Cond>, Box<Cond>),
-
-    // X || Y
-    Or(Box<Cond>, Box<Cond>),
-
-    // !X
-    Not(Box<Cond>),
-}
-
-impl Cond {
-    // Check that a condition is true given a certain character at the readhead and a certain counter value 
-    pub fn check(&self, read : Readable, counter : i32) -> bool {
-        match self {
-            // Compare the character at the readhead to the character in the condition
-            Cond::Read(char) => read == *char,
-            Cond::NotRead(char) => read != *char,
-
-            // Check the value of the counter
-            Cond::CheckZero() => counter == 0,
-            Cond::CheckNotZero() => counter != 0,
-
-            // Recurse on and statements
-            Cond::And(left, right) => left.check(read, counter) && right.check(read, counter),
-
-            // Recurse on or statements
-            Cond::Or(left, right) => left.check(read, counter) || right.check(read, counter),
-
-            // Recurse on not statements
-            Cond::Not(inner) => !inner.check(read, counter),
-        }
-    }
-
-    // Print the condition to the terminal
-    pub fn print(&self) {
-        match self {
-            // Print read conditions
-
-            Cond::Read(char) => {
-                print!("read == ");
-                char.print();
-            },
-
-            Cond::NotRead(char) => {
-                print!("read != ");
-                char.print();
-            },
-
-            // Print counter conditions
-            Cond::CheckZero() => print!("c == 0"),
-            Cond::CheckNotZero() => print!("c != 0"),
-
-            // Recurse on and statements
-            Cond::And(left, right) => {
-                print!("(");
-                left.print();
-                print!(") && (");
-                right.print();
-                print!(")");
-            },
-
-            // Recurse on or statements
-            Cond::Or(left, right) => {
-                print!("(");
-                left.print();
-                print!(") || (");
-                right.print();
-                print!(")");
-            },
-
-            // Recurse on not statements
-            Cond::Not(inner) => {
-                print!("!(");
-                inner.print();
-                print!(")");
-            }
-        }
     }
 }
