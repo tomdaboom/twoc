ea86cc87a83d14492a58693b536c1584789fcd91
Tom <tom.divers9@gmail.com>
2023-02-22 16:09:32 +0000

    stop cargo from whinging

--- src/proofs/mod.rs
+++ src/proofs/mod.rs
@@ -1,5 +1,7 @@
 // This mod implements the propositional calculus described in https://arxiv.org/abs/2101.11320
-
+#[allow(dead_code)]
 pub mod prop_calc;
+#[allow(dead_code)]
 pub mod fol;
+#[allow(dead_code)]
 pub mod arith;
