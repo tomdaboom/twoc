be184aecf4bef904a0bcf851ea830a5f914a71f0
Tom <tom.divers9@gmail.com>
2023-03-08 13:26:55 +0000

    proper cli

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 // IMPORTS
-use std::{env, fs};
+use std::fs;
 
 extern crate hashbrown;
 
@@ -20,16 +20,38 @@ use twoc::simulation::glueck::glueck_procedure;
 //use twoc::simulation::ahu::ahu_procedure;
 use twoc::simulation::naive_nondeterm::naive;
 
+// Clap import
+use clap::Parser;
+
+// Cli arguments
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct CliArgs {
+    #[arg(short, long)]
+    file : String,
+
+    #[arg(short, long)]
+    word : String,
+
+    #[arg(short, long, default_value_t = false)]
+    verbose : bool,
+}
+
 fn main() {
     // Declare parser for Twoc rule
     let parser = grammar_rules::TwocParser::new();
 
-    // Get name of file from command line args
-    let args : Vec<String> = env::args().collect();
-    let file_path = &args[1];
-    let test_word = &args[2];
-    println!("Parsing {:?}\n", file_path);
+    // Parse command line arguments 
+    let args = CliArgs::parse();
 
+    let file_path = &args.file;
+    let test_word = &args.word;
+    let verbose = args.verbose;
+
+    if verbose { 
+        println!("Parsing {:?}\n", file_path); 
+    }
+    
     // Load file
     let test_prog = fs::read_to_string(file_path).expect("File not found");
 
@@ -41,6 +63,12 @@ fn main() {
         Ok(prog) => prog,
     };
 
+    if verbose {
+        // Output sugared AST
+        println!("Sugared AST:");
+        sugar_prog.print();
+    }
+
     // Desugar the program
     let mut prog = convert_sugar(sugar_prog);
 
@@ -49,31 +77,37 @@ fn main() {
         panic!("{:?} contains characters that aren't in the program's alphabet!", test_word);
     }
 
-    // Output if the program is deterministic or not
-    match prog.deterministic() {
-        true  => print!("Deterministic"),
-        false => print!("Nondeterministic"),
-    }
-    println!(" program detected\n");
+    if verbose {
+        // Output if the program is deterministic or not
+        match prog.deterministic() {
+            true  => print!("\nDeterministic"),
+            false => print!("\nNondeterministic"),
+        }
+        println!(" program detected\n");
 
-    // Print AST
-    println!("AST:");
-    prog.print();
+        // Print AST
+        println!("AST:");
+        prog.print();
+    }
 
     if prog.deterministic() {
         // Contract AST
         prog.contract();
 
-        // Print contracted AST
-        println!("\nContracted AST:");
-        prog.print();
-
+        if verbose {
+            // Print contracted AST
+            println!("\nContracted AST:");
+            prog.print();
+        }
+        
         // Construct the automaton from the program
         let autom = determ_construction::construct_from_prog(prog);
 
         // Print the automaton
-        println!("\nAutomaton:");
-        autom.print();
+        if verbose {
+            println!("\nAutomaton:");
+            autom.print();
+        }
 
         // Test that the automaton accepts an example word via the glueck procedure
         let accepting = glueck_procedure(&autom, test_word);
@@ -87,10 +121,12 @@ fn main() {
         // Construct the automaton from the program
         let autom = construction::construct_from_prog(prog);
 
-        // Print the automaton
-        println!("\nAutomaton:");
-        autom.print();
-
+        if verbose {
+            // Print the automaton
+            println!("\nAutomaton:");
+            autom.print();
+        }
+        
         // Test that the automaton accepts an example word via the glueck procedure
         let accepting = naive(&autom, test_word);
 
