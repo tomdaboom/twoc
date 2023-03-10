0a166d3d775333c1c0a1bb108ad4ef8c0dacb8f2
Tom <tom.divers9@gmail.com>
2023-03-10 18:41:26 +0000

    added linear regression plot

--- tests/bench_results/result_plotter.py
+++ tests/bench_results/result_plotter.py
@@ -17,10 +17,12 @@ with open(sys.argv[1], 'r') as file:
 dts[0] = 0
 
 linreg_model = linregress(ns, ts)
+linear_pred  = [linreg_model.intercept + linreg_model.slope*i for i in ns]
 
 print(linreg_model)
 
 fig, ax = plt.subplots(1, 2, figsize=(15, 5))
 ax[0].plot(ns, ts)
+ax[0].plot(ns, linear_pred, 'r', label = f"R^2 value = {linreg_model.rvalue**2}")
 ax[1].plot([ns[i] for i in range(len(ns)) if dts[i] < 5], [i for i in dts if i < 5])
 plt.show()
