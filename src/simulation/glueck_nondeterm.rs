1e63ac5ac27c3c71235c3ac3c390ccbe54f17174
Tom <tom.divers9@gmail.com>
2023-03-11 21:21:37 +0000

    nondeterm glueck procedure done (not tested)

--- src/simulation/glueck_nondeterm.rs
+++ src/simulation/glueck_nondeterm.rs
@@ -1,7 +1,7 @@
 use hashbrown::HashMap;
 
-use crate::automaton::determ_autom::Autom;
-use crate::simulation::config::{Config, DeltaConfig, StrippedConfig, strip_config, make_delta_config, next, get_transition};
+use crate::automaton::autom::Autom;
+use crate::simulation::config::{Config, DeltaConfig, StrippedConfig, strip_config, make_delta_config, next_nondeterm, get_transitions};
 use crate::parser::ast::{Readable, Input};
 
 // Check if a string is accepted by a deterministic automaton using the glueck procedure
@@ -19,21 +19,24 @@ pub fn glueck_procedure<'a>(autom : &'a Autom, input : &str) -> bool {
     let mut simulator = GlueckSimulator::new(autom, readable_input);
 
     // Run the simulator to find the terminator of this config
-    let final_config = simulator.simulate(start_config);
+    let final_configs = simulator.simulate(start_config);
 
     //println!("\n{:?}", final_config);
 
-    // Return based on the final config
-    match autom.check_if_halting(final_config.state) {
-        None => false,
-        Some(accept) => accept,
+    for final_config in final_configs {
+        // Return based on the final config
+        if let Some(a) = autom.check_if_halting(final_config.state) {
+            if a { return true; }
+        }
     }
+    
+    false
 }
 
 // Struct to hold variables for the Glueck procedure
 struct GlueckSimulator<'a> {
     // Table that stores the previously computed terminators
-    config_table : HashMap<StrippedConfig, DeltaConfig>,
+    config_table : HashMap<StrippedConfig, Vec<DeltaConfig>>,
 
     // Automaton being simulated
     autom : &'a Autom,
@@ -57,116 +60,133 @@ impl<'a> GlueckSimulator<'a> {
     }
 
     // Find the terminator of a given configuration
-    pub fn simulate(&mut self, config : Config) -> Config {    
+    pub fn simulate(&mut self, config : Config) -> Vec<Config> {    
         let stripped_config = strip_config(config);
 
         // Check for infinite loops
         // If we loop infinitely, return the starting config
         if self.past_configs.contains(&stripped_config) {
-            return Config { state : 0, read : 0, counter : 0 };
+            return vec![];
         }
 
         // Record config in past configs stack
         self.past_configs.push(stripped_config);
 
         // Check if we've seen this configuration before
-        if let Some(delta_config) = self.config_table.get(&stripped_config) {
-            // Calculate the new config and return
-            return Config {
-                state : delta_config.state,
-                read : delta_config.read,
-                counter : config.counter + delta_config.counter,
-            };
+        if let Some(delta_configs) = self.config_table.get(&stripped_config) {
+            // Calculate the new configs and return
+            let mut new_configs = Vec::new();
+
+            for delta_config in delta_configs {
+                new_configs.push(Config {
+                    state : delta_config.state,
+                    read : delta_config.read,
+                    counter : config.counter + delta_config.counter,
+                });
+            }
+
+            return new_configs;
         }
 
         // Check if this configuration is halting
         if let Some(accepting) = self.autom.check_if_halting(config.state) {
             // Return if we are in a reject state or in an accept state with an empty counter
             if !accepting || (accepting && config.counter == 0) { 
-                return config; 
+                return vec![config]; 
             } 
         }
 
         // Find the legal transition from this config if one exists
-        let trans = match get_transition(self.autom, config, self.input.clone()) {
-            // If no such transition exists, then the automaton halts and rejects on this config
-            None => return config,
+        let transes = get_transitions(self.autom, config, self.input.clone());
 
-            // If such a transition exists, save it in trans
-            Some(t) => t,
-        };  
+        if transes.len() == 0 { return vec![config]; }  
 
         // Variable to hold the value the procedure should output
-        let out : Config;
+        let mut outs = Vec::new();
 
-        // Check if this transition is decrementing
-        // i.e. pop(config)
-        if trans.incr_by < 0 { 
-            // We've found the terminator (yaaaayyyyyyyyyyyy!!!!!!!!!)
-            out = config;
-        }
-        
-        // Check if this transition is incrementing
-        // i.e. push(config)
-        else if trans.incr_by > 0 {  
-            // Find the next configuration
-            let next_config = match next(
-                config, 
-                trans.clone(), 
-                self.input.clone(),
-                self.autom.decr_zero
-            ) {
-                None => return config,
-                Some(c) => c,
-            };
+        for trans in transes {
+            let mut out = Vec::new() ;
 
-            // Find the terminator of the next configuration
-            let next_terminator = self.simulate(next_config);
+            // Check if this transition is decrementing
+            // i.e. pop(config)
+            if trans.incr_by < 0 { 
+                // We've found the terminator (yaaaayyyyyyyyyyyy!!!!!!!!!)
+                out.push(config);
+            }
+            
+            // Check if this transition is incrementing
+            // i.e. push(config)
+            else if trans.incr_by > 0 { 
+                // Find the next configuration
+                let next_config = match next_nondeterm(
+                    config, 
+                    trans.clone(), 
+                    self.input.clone(),
+                    self.autom.decr_zero
+                ) {
+                    None => continue,
+                    Some(c) => c,
+                };
 
-            // Find the legal transition off of the next terminator if one exists
-            let next_terminator_trans = match get_transition(self.autom, next_terminator, self.input.clone()) {
-                None => return next_terminator,
-                Some(t) => t,
-            };  
+                // Find the terminator of the next configuration
+                let next_terminators = self.simulate(next_config);
 
-            // Find the configuration following the last terminator
-            let follow = match next(
-                next_terminator, 
-                next_terminator_trans,
-                self.input.clone(),
-                self.autom.decr_zero
-            ) {
-                None => return next_terminator,
-                Some(c) => c,
-            };
+                let mut follow = Vec::new();
 
-            // Recurse
-            out = self.simulate(follow);
-        } 
-        
-        // op(config)
-        else {
-            // Find the next configuration
-            let next_config = match next(
-                config, 
-                trans.clone(), 
-                self.input.clone(),
-                self.autom.decr_zero
-            ) {
-                None => return config,
-                Some(c) => c,
-            };
+                for next_terminator in next_terminators {
+                    // Find the legal transitions off of the next terminator if one exists
+                    let next_terminator_transes = get_transitions(self.autom, next_terminator, self.input.clone());  
+                    
+                    for next_terminator_trans in next_terminator_transes {
+                        // Find the configuration following the last terminator
+                        match next_nondeterm(
+                            next_terminator, 
+                            next_terminator_trans,
+                            self.input.clone(),
+                            self.autom.decr_zero
+                        ) {
+                            None => continue,
+                            Some(c) => follow.push(c),
+                        };
+                    }
+                }
 
-            // Recurse
-            out = self.simulate(next_config);
+                // Recurse
+                for cfg in follow {
+                    let mut new_cfgs = self.simulate(cfg);
+                    out.append(&mut new_cfgs);
+                }
+            } 
+            
+            // op(config)
+            else {
+                // Find the next configuration
+                let next_config = match next_nondeterm(
+                    config, 
+                    trans.clone(), 
+                    self.input.clone(),
+                    self.autom.decr_zero
+                ) {
+                    None => continue,
+                    Some(c) => c,
+                };
+
+                // Recurse
+                out = self.simulate(next_config);
+            }
+
+            outs.append(&mut out);
         }
 
         // Memoize
-        let map_config = make_delta_config(config, out);
-        self.config_table.insert(stripped_config, map_config);
+        let mut new_delta_configs = Vec::new();
+        for out in &outs {
+            new_delta_configs.push(make_delta_config(config, *out));
+        }
+        self.config_table.insert(stripped_config, new_delta_configs);
 
         // Return
         self.past_configs.pop();
-        out
+        outs
     }
 }
