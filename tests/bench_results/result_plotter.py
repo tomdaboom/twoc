2351c758acf3d08ac4a251e44a0929711e7d5db1
Tom Divers <Tom.Divers@warwick.ac.uk>
2023-03-12 16:04:53 +0000

    fiddling

--- tests/bench_results/result_plotter.py
+++ tests/bench_results/result_plotter.py
@@ -18,20 +18,21 @@ dts[0] = 0
 
 linreg_model = linregress(ns, ts)
 linear_pred  = [linreg_model.intercept + linreg_model.slope*i for i in ns]
-
 r_sq = linreg_model.rvalue**2
-
 print(f"r^2 value = {r_sq}")
 
 fig, ax = plt.subplots(1, 2, figsize=(15, 5))
 
-ax[0].set_xlabel("Input length")
-ax[0].set_ylabel("Simulation runtime (s)")
+plt.rcParams.update({'font.size': 16})
 
 ax[0].plot(ns, ts, 'b', label = f"Benchmark results")
 ax[0].plot(ns, linear_pred, 'r--', label = f"Linear regression (r^2 = {r_sq.round(3)})")
-
+ax[0].set_label("Input length")
+ax[0].set_ylabel("Simulation runtime (s)")
 ax[0].legend()
 
 ax[1].plot([ns[i] for i in range(len(ns)) if dts[i] < 5], [i for i in dts if i < 5])
+ax[1].set_xlabel("Input length")
+ax[1].set_ylabel("dt/dn (s)")
+
 plt.show()
