3d06379a14929c04421d9c340911b48d4e904175
Tom <tom.divers9@gmail.com>
2023-03-13 17:55:03 +0000

    ugh

--- tests/bench_results/result_plotter.py
+++ tests/bench_results/result_plotter.py
@@ -26,14 +26,16 @@ else:
     raise Exception("argv[1] should be lin or cub")
 
 # regression on (n, t)
-linreg_model = np.poly1d(np.polyfit(ns, ts, deg))
-linear_pred  = [linreg_model(i) for i in ns]
-r_sq = r2_score(ts, linear_pred)
-print(f"r^2 value = {r_sq}")
+t_model = np.poly1d(np.polyfit(ns, ts, deg))
+ts_pred  = t_model(ns)
+r_sq = r2_score(ts, ts_pred)
+print(f"t: r^2 value = {r_sq}")
 
 # regression on (n, dt)
-poly_model = np.poly1d(np.polyfit(ns, dts, deg-1))
-poly_pred  = [poly_model(i) for i in ns] 
+dt_model = np.poly1d(np.polyfit(ns, dts, deg-1))
+dts_pred  = dt_model(ns)
+dr_sq = r2_score(dts, dts_pred)
+print(f"dt: r^2 value = {dr_sq}")
 
 # PLOTTING
 
@@ -42,13 +44,13 @@ fig, ax = plt.subplots(1, 2, figsize=(15, 5))
 plt.rcParams.update({'font.size': 16})
 
 ax[0].plot(ns, ts, 'b', label = f"Benchmark results")
-ax[0].plot(ns, linear_pred, 'r--', label = f"Regression (r^2 = {r_sq.round(3)})")
+ax[0].plot(ns, ts_pred, 'r--', label = f"Regression (r^2 = {r_sq.round(3)})")
 ax[0].set_label("Input length")
 ax[0].set_ylabel("Simulation runtime (s)")
 ax[0].legend()
 
 ax[1].plot([ns[i] for i in range(len(ns)) if dts[i] < 5], [i for i in dts if i < 5], 'b', label = f"Benchmark derivative")
-ax[1].plot(ns, poly_pred, 'r--', label = f"Prediction")
+ax[1].plot(ns, dts_pred, 'r--', label = f"Regression (r^2 = {dr_sq.round(3)})")
 ax[1].set_xlabel("Input length")
 ax[1].set_ylabel("dt/dn (s)")
 ax[1].legend()
