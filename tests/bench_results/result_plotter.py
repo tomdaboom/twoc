81b9d130b758f014676010ff3a7d4a6317452849
Tom <tom.divers9@gmail.com>
2023-03-13 17:38:11 +0000

    even spicier bench

--- tests/bench_results/result_plotter.py
+++ tests/bench_results/result_plotter.py
@@ -10,7 +10,7 @@ ts  = []
 dts = []
 
 #read result file
-with open(sys.argv[1], 'r') as file:
+with open(sys.argv[2], 'r') as file:
     for line in file:
         split = line.split(",")
         ns.append(int(split[0]))
@@ -19,14 +19,24 @@ with open(sys.argv[1], 'r') as file:
 dts[0] = 0
 
 #linear regression on (n, t)
-linreg_model = np.poly1d(np.polyfit(ns, ts, 1))
+
+
+if sys.argv[1] == 'lin':
+    deg = 1
+elif sys.argv[1] == 'cub':
+    deg = 3
+else:
+    raise Exception("argv[1] should be lin or cub")
+
+linreg_model = np.poly1d(np.polyfit(ns, ts, deg))
+
+
 linear_pred  = [linreg_model(i) for i in ns]
 r_sq = r2_score(ts, linear_pred)
 print(f"r^2 value = {r_sq}")
 
 #poly regression on (n, dt)
-deg = 10
-poly_model = np.poly1d(np.polyfit(ns, dts, deg))
+poly_model = np.poly1d(np.polyfit(ns, dts, deg-1))
 poly_pred  = [poly_model(i) for i in ns] 
 
 fig, ax = plt.subplots(1, 2, figsize=(15, 5))
@@ -40,7 +50,7 @@ ax[0].set_ylabel("Simulation runtime (s)")
 ax[0].legend()
 
 ax[1].plot([ns[i] for i in range(len(ns)) if dts[i] < 5], [i for i in dts if i < 5], 'b', label = f"Benchmark derivative")
-ax[1].plot(ns, poly_pred, 'r--', label = f"Polyfit prediction (degree = {deg})")
+ax[1].plot(ns, poly_pred, 'r--', label = f"Prediction")
 ax[1].set_xlabel("Input length")
 ax[1].set_ylabel("dt/dn (s)")
 ax[1].legend()
