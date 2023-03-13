04685e8c42dbc8e250a079c6fce1b3b49fe87c74
Tom <tom.divers9@gmail.com>
2023-03-13 17:39:36 +0000

    comments

--- tests/bench_results/result_plotter.py
+++ tests/bench_results/result_plotter.py
@@ -18,9 +18,6 @@ with open(sys.argv[2], 'r') as file:
         dts.append(float(split[2]))
 dts[0] = 0
 
-#linear regression on (n, t)
-
-
 if sys.argv[1] == 'lin':
     deg = 1
 elif sys.argv[1] == 'cub':
@@ -28,23 +25,24 @@ elif sys.argv[1] == 'cub':
 else:
     raise Exception("argv[1] should be lin or cub")
 
+# regression on (n, t)
 linreg_model = np.poly1d(np.polyfit(ns, ts, deg))
-
-
 linear_pred  = [linreg_model(i) for i in ns]
 r_sq = r2_score(ts, linear_pred)
 print(f"r^2 value = {r_sq}")
 
-#poly regression on (n, dt)
+# regression on (n, dt)
 poly_model = np.poly1d(np.polyfit(ns, dts, deg-1))
 poly_pred  = [poly_model(i) for i in ns] 
+
+# PLOTTING
 
 fig, ax = plt.subplots(1, 2, figsize=(15, 5))
 
 plt.rcParams.update({'font.size': 16})
 
 ax[0].plot(ns, ts, 'b', label = f"Benchmark results")
-ax[0].plot(ns, linear_pred, 'r--', label = f"Linear regression (r^2 = {r_sq.round(3)})")
+ax[0].plot(ns, linear_pred, 'r--', label = f"Regression (r^2 = {r_sq.round(3)})")
 ax[0].set_label("Input length")
 ax[0].set_ylabel("Simulation runtime (s)")
 ax[0].legend()
