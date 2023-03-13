7a12b66e961f3ff65da9e3d9cc3901c5eaa9fff8
Tom <tom.divers9@gmail.com>
2023-03-13 17:38:26 +0000

    minor changes

--- tests/nondeterm_bench.rs
+++ tests/nondeterm_bench.rs
@@ -1,6 +1,7 @@
 // Import grammar
 #[macro_use] extern crate lalrpop_util;
 lalrpop_mod!(pub grammar_rules, "/parser/grammar_rules.rs");
+lalrpop_mod!(pub sugar_grammar, "/parser/sugar/sugar_grammar.rs");
 
 #[cfg(test)]
 mod nondeterm_bench {
@@ -9,13 +10,15 @@ mod nondeterm_bench {
     use std::time::Instant;
 
     use crate::grammar_rules::TwocParser;
+    use crate::sugar_grammar::TwocParser as SugarTwocParser;
+    use twoc::parser::sugar::convert_sugar::convert_sugar;
     use twoc::automaton::construction; 
     use twoc::automaton::autom::Autom;
     use twoc::simulation::glueck_nondeterm::glueck_procedure;
 
     // Function used by threads
     pub fn thread_function(autom : &Autom, word : String) {
-        assert_eq!(glueck_procedure(&autom, &word), true);
+        glueck_procedure(&autom, &word);
     }
 
     #[test]
@@ -106,6 +109,70 @@ mod nondeterm_bench {
             Err(ref err) => panic!("Parse Error:\n{:?}", err),
             Ok(prog) => prog,
         };
+
+        // Contract the AST
+        prog.contract();
+
+        // Construct the automaton from the program
+        let autom = construction::construct_from_prog(prog);
+
+        for n in (start..(start + step*tests + 1)).step_by(step) {
+            // Generate a string of n 0s and n 1s
+            let test_word = "0".repeat(n);
+
+            // Start timing
+            let now = Instant::now();
+
+            // Run test with a massive stack
+            thread::scope(|s| {
+                thread::Builder::new().stack_size(0xFFFF * n)
+                .spawn_scoped(s, || thread_function(&autom, test_word))
+                .unwrap();
+            });
+
+            // Stop timing and record delta t
+            let time_taken = now.elapsed().as_secs_f32();
+            let delta_t = time_taken - last;
+
+            // Output and save time taken and difference between last time and this time
+            println!("n = {:?}, t = {:?}, Δt = {:?}", n, time_taken, delta_t);
+            let to_file = format!("{:?},{:?},{:?}\n", n, time_taken, delta_t);
+            file.write_all(to_file.as_bytes()).expect("File write failed");
+
+            last = time_taken;
+        }
+    }
+
+    #[test]
+    pub fn awful_performnce_test() {
+        // Loop params
+        let start = 0;
+        let step = 10;
+        let tests = 1000;
+
+        // Initialise last
+        let mut last = 0.0f32;
+
+        // Create output file
+        let path = "./tests/bench_results/nondeterm_torture.txt";
+        let mut file = fs::File::create(path).expect("File creation failed");
+
+        // Declare parser for Twoc rule
+        let parser = SugarTwocParser::new();
+
+        // Load file
+        let test_prog = fs::read_to_string("./twocprogs/nondeterministic/branch_while.twoc").expect("File not found");
+
+        // Parse the file
+        let test = parser.parse(&test_prog);
+        let sugar_prog = match test {
+            // Output any parse errors
+            Err(ref err) => panic!("Parse Error:\n{:?}", err),
+            Ok(prog) => prog,
+        };
+
+        // Desugar
+        let mut prog = convert_sugar(sugar_prog);
 
         // Contract the AST
         prog.contract();
