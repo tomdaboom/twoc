3dbc220d3c5005cf2bcd5a9dc68aecc3022c0a26
Tom Divers <Tom.Divers@warwick.ac.uk>
2023-04-24 12:29:37 +0100

    old autom and construction deprecated

--- src/automaton/mod.rs
+++ src/automaton/mod.rs
@@ -1,8 +1,8 @@
 pub mod generic_autom;
-pub mod autom;
+//pub mod autom;
 pub mod determ_autom;
 
-pub mod construction_old;
+//pub mod construction_old;
 pub mod determ_construction;
 pub mod construction;
 
